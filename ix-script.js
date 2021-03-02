093311f45ebaee582b25cd982f1bcf90e3215a92
Francois <couturefrancois97@hotmail.com>
2021-03-02 19:01:34 -0400

    big update

--- ix-script.js
+++ ix-script.js
@@ -11,42 +11,88 @@ window.onload = function() {
   
 }
 
-window.setInterval(function(){
-    
-    if (getComputedStyle(highWindow).getPropertyValue("top") == "-90px"){
-      ddWindow.style.opacity = "1"; 
-    } else {
-      ddWindow.style.opacity = "0"; 
-    }
-}, 100)
 
-window.onscroll = function() {
+
+window.setInterval(function(){
+  if (getComputedStyle(highWindow).getPropertyValue("top") == "-90px"){
+    ddWindow.style.transition = "opacity 0.2s";
+    ddWindow.style.opacity = "1"; 
+      
+  }
+}, 750);
+
+function setNavPos(){
   var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos != currentScrollPos && currentScrollPos > 100) {
-    highWindow.style.transition = "all 1s";
-    highWindow.style.transitionDelay = "0.75s";
-    highWindow.style.top = "-90px"; 
+  if (prevScrollpos != currentScrollPos && scrollY > 100) {
+    highWindow.style.transition = "top 1s";
+    highWindow.style.transitionDelay = "0.5s";
+    highWindow.style.top = "-90px";
+    ddWindow.style.transition = "top 1s";
+    ddWindow.style.transitionDelay = "0.5s";
+    ddWindow.style.top = "-1%";
+    
+    
      
   } else if(currentScrollPos < 200){
     highWindow.style.transition = "all 0.5s";
     highWindow.style.transitionDelay = "0s";
     highWindow.style.top = "0";
-     
+    
+    
+    ddWindow.style.transition = "top 0.5s, opacity 0.75s";
+    ddWindow.style.transitionDelay = "0s";
+    ddWindow.style.top = "90px";
+    ddWindow.style.opacity = "0";
+
   }
   prevScrollpos = currentScrollPos;
-} 
-highWindow.onmouseover = function(){
-  if(highWindow.style.top == "-90px") {
-    highWindow.style.cursor = "pointer"; 
-  }
-  else {
-    highWindow.style.cursor = "default";  
-  }
   
 }
-highWindow.onclick = function(){
+window.onscroll = function() {
+  setNavPos();
+}
+
+/* function */
+function canClick(item) {
+  if(highWindow.style.top == "-90px") {
+    return item.style.cursor = "pointer";
+    
+  }
+  else {
+    return item.style.cursor = "default";
+    
+  }  
+}
+
+/* Applies to */
+ddWindow.onmouseover = function(){
+  canClick(ddWindow);
+};
+highWindow.onmouseover = function(){
+  canClick(highWindow);
+}
+/**/
+
+/* Function */
+function expandNav(){
   highWindow.style.transition = "all 0.5s";
   highWindow.style.transitionDelay = "0s";
   highWindow.style.top = "0";
+  
+  ddWindow.style.transition = "all 0.5s";
+  ddWindow.style.transitionDelay = "0s";
+  ddWindow.style.top = "90px";
 }
 
+/* Applies to */
+highWindow.onclick = function(){
+  expandNav();
+  canClick(highWindow);
+  setNavPos();
+}
+ddWindow.onclick = function(){
+  expandNav();
+  canClick(ddWindow);
+  setNavPos();
+}
+/**/
