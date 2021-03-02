b44684d349ea6757c5148e7f80bddcd89fd9528d
Francois <couturefrancois97@hotmail.com>
2021-03-02 16:48:45 -0400

    changed logical operation
    changed logical operation for window.onscroll function

---
+++ ix-script.js
@@ -0,0 +1,52 @@
+var prevScrollpos = window.pageYOffset;
+var highWindow = document.getElementById("high")
+var ddWindow = document.getElementById("dd")
+
+window.onload = function() {
+  var currentScrollpos = window.pageYOffset;
+  if (currentScrollpos > 100) {
+    highWindow.style.top = "-90px";
+    ddWindow.style.opacity = "1";
+  }
+  
+}
+
+window.setInterval(function(){
+    
+    if (getComputedStyle(highWindow).getPropertyValue("top") == "-90px"){
+      ddWindow.style.opacity = "1"; 
+    } else {
+      ddWindow.style.opacity = "0"; 
+    }
+}, 100)
+
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos != currentScrollPos && currentScrollPos > 100) {
+    highWindow.style.transition = "all 1s";
+    highWindow.style.transitionDelay = "0.75s";
+    highWindow.style.top = "-90px"; 
+     
+  } else if(currentScrollPos < 200){
+    highWindow.style.transition = "all 0.5s";
+    highWindow.style.transitionDelay = "0s";
+    highWindow.style.top = "0";
+     
+  }
+  prevScrollpos = currentScrollPos;
+} 
+highWindow.onmouseover = function(){
+  if(highWindow.style.top == "-90px") {
+    highWindow.style.cursor = "pointer"; 
+  }
+  else {
+    highWindow.style.cursor = "default";  
+  }
+  
+}
+highWindow.onclick = function(){
+  highWindow.style.transition = "all 0.5s";
+  highWindow.style.transitionDelay = "0s";
+  highWindow.style.top = "0";
+}
+
